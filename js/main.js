5bb2e5a1fc8cf44cb1fc9b7d854f32fa54b9a502
angeloesaki <angeloesaki@gmail.com>
2020-07-05 12:25:23 +0900

    Panelクラスを作成

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 "use strict";
 
 {
+  class Panel {
+    constructor() {
+      const section = document.createElement("section");
+      section.classList.add("panel");
+
+      //img要素とstopのdiv要素をこのクラスのプロパティーとする
+      this.img = document.createElement("img");
+      this.img.src = "img/seven.png";
+
+      this.stop = document.createElement("div");
+      this.stop.textContent = "STOP";
+      this.stop.classList.add("stop");
+    }
+  }
 }
